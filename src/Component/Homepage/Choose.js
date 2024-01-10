2754adad28280d936efad3b9249b12270e4e763a
SwanyEngeering <155013596+SwanyEngeering@users.noreply.github.com>
2024-01-10 22:19:11 +0500

    Add files via upload

--- src/Component/Homepage/Choose.js
+++ src/Component/Homepage/Choose.js
@@ -12,44 +12,47 @@ export default function Choose() {
         <div className={style.heading}>Why You Choose Us?</div>
       </div>
       <div className={style.cardContainer}>
-        <div className={style.card}>
+        <div className={`${style.card}`}>
           <div className={style.cardHead}>FLEXIBLE PROJECT TIMELINES</div>
           <div className={style.text}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-            ipsa ea fugit aperiam sapiente, deserunt facere sit harum repellat,
-            doloribus quis voluptatem dicta dignissimos, est porro. Maiores
-            facere rerum odio.
+            Flexible project timelines represent a contemporary approach to
+            project management that acknowledges the inherent uncertainties and
+            complexities in various industries. Unlike rigid, fixed schedules,
+            flexible timelines recognize the need for adaptability in the face
+            of unforeseen challenges, changes in requirements, or unexpected
+            opportunities.
           </div>
         </div>
-        <div className={style.card}>
+        <div className={`${style.card} ${style.solution}`}>
           <div className={style.cardHead}>
             TAILORED SOLUTIONS FOR EVERY CLIENT
           </div>
           <div className={style.text}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-            ipsa ea fugit aperiam sapiente, deserunt facere sit harum repellat,
-            doloribus quis voluptatem dicta dignissimos, est porro. Maiores
-            facere rerum odio.
+            Offering tailored solutions for every client is the hallmark of a
+            customer-centric approach that recognizes the unique needs and
+            preferences of individuals or businesses.
           </div>
         </div>
-        <div className={style.card}>
+        <div className={`${style.card} ${style.effort}`}>
           <div className={style.cardHead}>EFFORTLESS ONBOARDING PROCESS</div>
           <div className={style.text}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-            ipsa ea fugit aperiam sapiente, deserunt facere sit harum repellat,
-            doloribus quis voluptatem dicta dignissimos, est porro. Maiores
-            facere rerum odio.
+            An effortless onboarding process is a cornerstone of a positive
+            employee experience, setting the tone for a smooth integration into
+            a new work environment. Such a process is characterized by its
+            simplicity, clarity, and efficiency, minimizing stress and
+            maximizing engagement.
           </div>
         </div>
-        <div className={style.card}>
+        <div className={`${style.card} ${style.time}`}>
           <div className={style.cardHead}>
             TIMELY AND RESPONSIVE CUSTOMER SUPPORT
           </div>
           <div className={style.text}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis
-            ipsa ea fugit aperiam sapiente, deserunt facere sit harum repellat,
-            doloribus quis voluptatem dicta dignissimos, est porro. Maiores
-            facere rerum odio.
+            Timely and responsive customer support stands as the cornerstone of
+            a positive and enduring customer experience. In a fast-paced and
+            interconnected world, businesses that prioritize prompt and
+            effective customer service distinguish themselves in the
+            marketplace.
           </div>
         </div>
       </div>
